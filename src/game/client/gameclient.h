2a87990fec0589c6bfe6fcc8159fec9e092d56ab
oy <Tom_Adams@web.de>
2011-06-29 22:27:32 +0200

    made it possible to filter by player country in the server browser. Closes #654

--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -225,7 +225,6 @@ public:
 	virtual void OnStartGame();
 
 	virtual const char *GetItemName(int Type);
-	virtual int GetCountryIndex(int Code);
 	virtual const char *Version();
 	virtual const char *NetVersion();
 
