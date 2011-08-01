fe4bdf380e4b4f59e6e87618ca9f20b4c8313a70
SushiTee <weichel.sascha@xxx.xx>
2011-08-01 13:44:17 +0200

    fixed compiling error in teerace build

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -1713,12 +1713,20 @@ void CServer::DemoRecorder_HandleAutoStart()
 {
 	if(g_Config.m_SvAutoDemoRecord)
 	{
+#if defined(CONF_TEERACE)
+		m_aDemoRecorder[MAX_CLIENTS].Stop();
+#else
 		m_DemoRecorder.Stop();
+#endif
 		char aFilename[128];
 		char aDate[20];
 		str_timestamp(aDate, sizeof(aDate));
 		str_format(aFilename, sizeof(aFilename), "demos/%s_%s.demo", "auto/autorecord", aDate);
+#if defined(CONF_TEERACE)
+		m_aDemoRecorder[MAX_CLIENTS].Start(Storage(), m_pConsole, aFilename, GameServer()->NetVersion(), m_aCurrentMap, m_CurrentMapCrc, "server");
+#else
 		m_DemoRecorder.Start(Storage(), m_pConsole, aFilename, GameServer()->NetVersion(), m_aCurrentMap, m_CurrentMapCrc, "server");
+#endif
 		if(g_Config.m_SvAutoDemoMax)
 		{
 			// clean up auto recorded demos
