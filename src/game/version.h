e6ec5053ee35289562d1f1a8747ac5d80f6ca734
Redix <redix@hotmail.de>
2017-02-07 01:39:12 +0100

    Increment version number

--- src/game/version.h
+++ src/game/version.h
@@ -3,10 +3,10 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_VERSION "0.6.4"
+#define GAME_VERSION "0.6.4, Race " RACE_VERSION
 //#define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-#define RACE_VERSION "3.3-dev"
+#define RACE_VERSION "3.4"
 #define TEERACE_GAMESERVER_VERSION "1"
 static const char GAME_RELEASE_VERSION[8] = {'0', '.', '6', '.', '4', 0};
 #endif
