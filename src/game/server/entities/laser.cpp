ad546ef1557df63229a77513553dfc801b2000d5
Redix <redix@hotmail.de>
2017-09-02 17:40:29 +0200

    Fix 'show others' for laser

--- src/game/server/entities/laser.cpp
+++ src/game/server/entities/laser.cpp
@@ -98,7 +98,7 @@ void CLaser::TickPaused()
 
 void CLaser::Snap(int SnappingClient)
 {
-	if(NetworkClipped(SnappingClient))
+	if(NetworkClipped(SnappingClient) || (!GameServer()->m_apPlayers[SnappingClient]->m_ShowOthers && SnappingClient != m_Owner))
 		return;
 
 	CNetObj_Laser *pObj = static_cast<CNetObj_Laser *>(Server()->SnapNewItem(NETOBJTYPE_LASER, m_ID, sizeof(CNetObj_Laser)));
